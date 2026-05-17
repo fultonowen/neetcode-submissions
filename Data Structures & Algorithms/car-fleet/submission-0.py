9d174792cb0a1cdd48dc673ced854c8fd4a915f5
Owen Fulton <34138924+fultonowen@users.noreply.github.com>
2026-05-17 14:11:13 -0400

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        fleet_size = 0
+        
+        position2 = sorted([(i, v) for i, v in enumerate(position)], key=lambda x: x[1])
+
+        group_arrival_time = -1
+        for idx, init_pos in reversed(position2):
+            arrival_time = (target - init_pos) / speed[idx]
+            if arrival_time > group_arrival_time:
+                group_arrival_time = arrival_time
+                fleet_size +=1
+        return fleet_size
+
+
+
+        # for idx, initial in enumerate(position):
+            # arr[idx] = (target - initial) / speed[idx]
+        
+        return -1
