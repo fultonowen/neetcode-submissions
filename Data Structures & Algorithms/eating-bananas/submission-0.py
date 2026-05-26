127062da3b7064dff60192eb2d79fbe05d9a09c0
Owen Fulton <34138924+fultonowen@users.noreply.github.com>
2026-05-25 22:29:08 -0400

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def can_eat(rate: int) -> bool:
+            nonlocal piles, h
+            time = 0
+            for num in piles:
+                time += int(math.ceil(num / rate))
+            
+            return time <= h
+        
+        val = sum(piles) * h
+        l, r = 1, val
+
+        while l <= r:
+            mid = (l + r) // 2
+            if can_eat(mid) == True:
+                r = mid - 1
+            else:
+                l = mid + 1
+        
+        return l
